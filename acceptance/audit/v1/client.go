f523739cdf1f4ec75ea0b81286576abc90d492a3
kayrus <kay.diam@gmail.com>
2019-05-17 21:20:46 +0200

    Unify client auth in acceptance tests

--- acceptance/audit/v1/client.go
+++ acceptance/audit/v1/client.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/sapcc/hermes-ctl/audit"
+	"github.com/sapcc/hermes-ctl/client"
 )
 
 // configureDebug will configure the provider client to print the API
@@ -28,20 +29,22 @@ func configureDebug(client *gophercloud.ProviderClient) *gophercloud.ProviderCli
 // to the OpenStack Hermes v1 API. An error will be returned if
 // authentication or client creation was not possible.
 func NewHermesV1Client() (*gophercloud.ServiceClient, error) {
-	// Workaround for the "AuthOptionsFromEnv"
-	os.Setenv("OS_DOMAIN_NAME", os.Getenv("OS_PROJECT_DOMAIN_NAME"))
-
-	ao, err := openstack.AuthOptionsFromEnv()
+	ao, err := client.AuthOptionsFromEnv()
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := openstack.AuthenticatedClient(ao)
+	client, err := openstack.NewClient(ao.IdentityEndpoint)
 	if err != nil {
 		return nil, err
 	}
 
 	client = configureDebug(client)
+
+	err = openstack.Authenticate(client, ao)
+	if err != nil {
+		return nil, err
+	}
 
 	return audit.NewHermesV1(client, gophercloud.EndpointOpts{
 		Region: os.Getenv("OS_REGION_NAME"),
